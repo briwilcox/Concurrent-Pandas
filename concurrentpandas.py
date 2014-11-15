eb76e0790bad7a5baeb59daf9bdd5a20a3230991
briwilcox <briwilcox@gmail.com>
2014-11-15 16:45:51 -0600

    added module versioning string

--- concurrentpandas.py
+++ concurrentpandas.py
@@ -1,4 +1,5 @@
 __author__ = 'Brian M Wilcox'
+__version__ = '0.11'
 
 """
 
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*paths):
 
 setup(
     name='ConcurrentPandas',
-    version='0.1.0',
+    version='0.1.1',
     description='Download data using pandas with multi-threading and multi-processing.',
     long_description=(read('README.md') + '\n\n' +
                       read('AUTHORS.rst')) + '\n\n',
